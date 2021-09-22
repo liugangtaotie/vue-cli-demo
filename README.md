a9749b3472546f1b7d096e96d963ee48a3206631
互联网-lg <liugang2@bsoft.com.cn>
2021-09-22 17:59:14 +0800

    FIX:9.22修改cliv1.5

---
+++ README.md
@@ -0,0 +1,10 @@
+# 常见 npm 包
+
+- [commander](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Ftj%2Fcommander.js)：提供了用户命令行输入和参数解析强大功能。
+- [Inquirer](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2FSBoudrias%2FInquirer.js)：交互式命令行工具
+- [execa](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fsindresorhus%2Fexeca)：A better [child_process](https://link.juejin.cn?target=https%3A%2F%2Fnodejs.org%2Fapi%2Fchild_process.html)。
+- [handlebars](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fwycats%2Fhandlebars.js)：一个 javascript 语以模版库。
+- [metalsmith](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fsegmentio%2Fmetalsmith)；An extremely simple, pluggable static site generator。
+- [chalk](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fchalk%2Fchalk)：Terminal string styling done right。
+- [download-git-repo](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fflipxfx%2Fdownload-git-repo)：Download and extract a git repository (GitHub, GitLab, Bitbucket) from node。
+- [consolidate](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Ftj%2Fconsolidate.js)：Template engine consolidation library for node.js 。
--- vue-auto-cli/lib/init.js
+++ vue-auto-cli/lib/init.js
@@ -2,13 +2,14 @@ const { promisify } = require('util')
 const clear = require('clear')
 const chalk = require('chalk')
 const figlet = promisify(require('figlet'))
-const log = (str) => console.info(chalk.green(str))
+const logChalk = (str) => console.info(chalk.green(str))
 const download = require('download-git-repo')
+const log = require('./log')
 
 module.exports = async (name) => {
   clear()
   const data = await figlet('bs-cli welcome')
-  log(data)
+  logChalk(data)
 
   const repo =
     'direct:https://github.com/liugangtaotie/vue2-mobile-ssr-code.git#main'
@@ -17,7 +18,7 @@ module.exports = async (name) => {
 
   const process = ora({
     text: '加载中',
-    prefixText: '🚴🏻下载模板->',
+    prefixText: '🚴🏻Download from template...',
     color: 'gray',
   })
 
@@ -25,10 +26,28 @@ module.exports = async (name) => {
 
   await download(repo, name, { clone: true }, (err) => {
     // 个人分析出的两种错误情况：1 同目录项目名已存在 2 储存库出现问题
-    if (err) return console.log('项目已存在或存储库错误！')
+    if (err) return process.fail('项目已存在或存储库错误！')
     // 创建成功 提示
     setTimeout(() => {
-      process.succeed(chalk.green('vue项目创建成功'))
+      process.succeed(chalk.green('模板下载成功'))
+      console.log(
+        `
+        ▄████ ▓█████▄  ██▓███    ▄████     ▄████▄   ██▓     ██▓
+        ██▒ ▀█▒▒██▀ ██▌▓██░  ██▒ ██▒ ▀█▒   ▒██▀ ▀█  ▓██▒    ▓██▒
+        ▒██░▄▄▄░░██   █▌▓██░ ██▓▒▒██░▄▄▄░   ▒▓█    ▄ ▒██░    ▒██▒
+        ░▓█  ██▓░▓█▄   ▌▒██▄█▓▒ ▒░▓█  ██▓   ▒▓▓▄ ▄██▒▒██░    ░██░
+        ░▒▓███▀▒░▒████▓ ▒██▒ ░  ░░▒▓███▀▒   ▒ ▓███▀ ░░██████▒░██░
+        ░▒   ▒  ▒▒▓  ▒ ▒▓▒░ ░  ░ ░▒   ▒    ░ ░▒ ▒  ░░ ▒░▓  ░░▓  
+        ░   ░  ░ ▒  ▒ ░▒ ░       ░   ░      ░  ▒   ░ ░ ▒  ░ ▒ ░
+        ░ ░   ░  ░ ░  ░ ░░       ░ ░   ░    ░          ░ ░    ▒ ░
+            ░    ░                   ░    ░ ░          ░  ░ ░  
+                ░                          ░                                                                                                                                                              
+    `
+      )
+      log.success(`🎉  Successfully created project ${name}.`)
+      log.success('👉  Get started with the following commands:\n')
+      log.bash(`cd ${name}`)
+      log.bash(`yarn install`)
     }, 50)
   })
 }
