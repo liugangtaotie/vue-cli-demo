bb22480ace3b66217b321a1ac00bc5e3a68b240f
互联网-lg <liugang2@bsoft.com.cn>
2021-09-22 12:58:12 +0800

    FIX:9.22修改cliv1.1

---
+++ vue-cli-test/lib/init.js
@@ -0,0 +1,30 @@
+const { promisify } = require('util')
+const clear = require('clear')
+const chalk = require('chalk')
+const figlet = promisify(require('figlet'))
+const log = (str) => console.info(chalk.green(str))
+const download = require('download-git-repo')
+
+module.exports = async (name) => {
+  clear()
+  const data = await figlet('bsm welcom')
+  log(data)
+
+  const repo =
+    'direct:https://github.com/liugangtaotie/vue2-mobile-ssr-code.git#main'
+
+  const ora = require('ora')
+
+  const process = ora(`🚴🏻下载....${repo}`)
+
+  process.start()
+
+  await download(repo, name, { clone: true }, (err) => {
+    // 个人分析出的两种错误情况：1 同目录项目名已存在 2 储存库出现问题
+    if (err) return console.log('项目已存在或存储库错误！')
+    // 创建成功 提示
+    console.log('vue项目创建成功')
+  })
+
+  process.succeed()
+}
