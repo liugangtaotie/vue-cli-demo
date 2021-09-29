7d5332d609517ea77e027dc1bfee3f51116656d2
互联网-lg <liugang2@bsoft.com.cn>
2021-09-29 16:10:32 +0800

    FIX:9.29修改cliv1.5

--- docorate-example/index.js
+++ docorate-example/index.js
@@ -1,17 +1,28 @@
+// @docorate anotation 注解风格的装饰器
 class Log {
   print(msg) {
     console.info(msg)
   }
 }
 
-const dec = (target, property) => {
-  const old = target.prototype[property]
-  target.prototype[property] = (msg) => {
-    old(`{${msg}}`)
+function decorate(target, property, descriptor) {
+  const oldValue = descriptor.value
+  descriptor.value = (msg) => {
+    return oldValue.apply(null, [`[${msg}]`])
   }
+  return descriptor
 }
 
-dec(Log, 'print')
+const anotation = (target, proterty, decorate) => {
+  const descriptor = decorate(
+    target.prototype,
+    proterty,
+    Object.getOwnPropertyDescriptor(target.prototype, proterty)
+  )
+  Object.defineProperty(target.prototype, proterty, descriptor)
+}
+
+anotation(Log, 'print', decorate)
 
 const log = new Log()
 
