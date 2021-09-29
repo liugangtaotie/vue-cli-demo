8bd59a08611d4481ced0cb60cf2ad440f9c363b6
互联网-lg <liugang2@bsoft.com.cn>
2021-09-29 13:50:03 +0800

    FIX:9.29修改cliv1.3

--- README.md
+++ README.md
@@ -1,36 +1,18 @@
 # 常见 npm 包
 
-- [commander](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Ftj%2Fcommander.js)：提供了用户命令行输入和参数解析强大功能。
-- [Inquirer](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2FSBoudrias%2FInquirer.js)：交互式命令行工具
-- [execa](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fsindresorhus%2Fexeca)：A better [child_process](https://link.juejin.cn?target=https%3A%2F%2Fnodejs.org%2Fapi%2Fchild_process.html)。
-- [handlebars](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fwycats%2Fhandlebars.js)：一个 javascript 语以模版库。
-- [metalsmith](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fsegmentio%2Fmetalsmith)；An extremely simple, pluggable static site generator。
-- [chalk](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fchalk%2Fchalk)：Terminal string styling done right。
-- [download-git-repo](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fflipxfx%2Fdownload-git-repo)：Download and extract a git repository (GitHub, GitLab, Bitbucket) from node。
-- [consolidate](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Ftj%2Fconsolidate.js)：Template engine consolidation library for node.js 。
-
-`commander` 命令行界面完整解决方案。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Ftj%2Fcommander.js%2Fblob%2FHEAD%2FReadme_zh-CN.md)
-
-`inquirer` 交互式命令界面集合。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2FSBoudrias%2FInquirer.js%23readme)
-
-`globby` 路径匹配工具。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fsindresorhus%2Fglobby%23readme)
-
-`execa` 子进程管理工具。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fsindresorhus%2Fexeca%23readme)
-
-`fs-extra`加强版文件系统。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fjprichardson%2Fnode-fs-extra)
-
-`download-git-repo`下载代码仓库工具。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgitlab.com%2Fflippidippi%2Fdownload-git-repo%23readme)
-
-`ejs`模版渲染。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fmde%2Fejs)
-
-`chalk`终端字符样式。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fchalk%2Fchalk%23readme)
-
-`boxen`终端“盒子”。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fsindresorhus%2Fboxen%23readme)
-
-`vue-codemod`将文件内容转换成**AST**。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fvuejs%2Fvue-codemod%23readme)
-
-`ora`终端loading效果。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fsindresorhus%2Fora%23readme)
-
-`figlet`打印终端logo。[传送门🚪](https://link.juejin.cn?target=https%3A%2F%2Fgithub.com%2Fpatorjk%2Ffiglet.js%23readme)
-
-`open`跨平台打开链接。[传送门🚪](
+- [commander](https://github.com/tj/commander.js)：提供了用户命令行输入和参数解析强大功能。
+- [Inquirer](https://github.com/SBoudrias/Inquirer.js)：交互式命令行工具
+- [execa](https://github.com/sindresorhus/execa)：A better [child_process](https://github.com/patrick-steele-idem/child-process-promise)。
+- [handlebars](https://github.com/handlebars-lang/handlebars.js)：一个 javascript 语以模版库。
+- [metalsmith](https://github.com/segmentio/metalsmith)；An extremely simple, pluggable static site generator。
+- [chalk](https://github.com/chalk/chalk)：Terminal string styling done right。
+- [consolidate](https://github.com/tj/consolidate.js)：Template engine consolidation library for node.js 。
+- [fs-extra](https://github.com/jprichardson/node-fs-extra):
+- [globby](https://github.com/sindresorhus/globby):User-friendly glob matching
+- [boxen](https://github.com/boxen/boxen):
+- [ora](https://github.com/sindresorhus/ora):Elegant terminal spinner
+- [figlet](https://github.com/cmatsuoka/figlet):a program that creates large characters out of ordinary
+screen characters
+- [open](https://github.com/sindresorhus/open):like URLs, files, executables. Cross-platform.
+- [Yargs](https://github.com/yargs/yargs):a node.js library fer hearties tryin' ter parse optstrings
+- [nanocolors](https://github.com/ai/nanocolors):A tiny and fast Node.js library to ANSI colors to terminal output.
--- cli-example/0926-cli/libs/init.js
+++ cli-example/0926-cli/libs/init.js
@@ -8,6 +8,7 @@ const log = (name) => {
 }
 const open = require('open')
 const download = require('download-git-repo')
+const { green, bold } = require('nanocolors')
 
 module.exports = async (name) => {
   clear()
@@ -16,34 +17,127 @@ module.exports = async (name) => {
 
   log(data)
 
-  const repo =
-    'direct:https://bitbucket.org/JeremyYu_1122/vue-test/get/master.zip'
+  console.log(green(`Task ${bold('1')} was finished`))
 
-  const process = ora('下载模板....')
+  // const readline = require('readline')
+  // const question = ['请输入您的姓名', '请输入您的年龄']
+  // const result = []
+  // const rl = readline.createInterface({
+  //   input: process.stdin,
+  //   output: process.stdout,
+  //   prompt: `？${question[0]} `,
+  // })
+  // rl.prompt()
 
-  process.start()
+  // rl.on('line', (line) => {
+  //   result.push(line.trim())
+  //   const max = result.length
+  //   if (max === question.length) {
+  //     rl.close()
+  //   }
+  //   rl.setPrompt(`？${question[max]} `)
+  //   rl.prompt()
+  // }).on('close', () => {
+  //   console.log(`谢谢参与问答 *** 姓名: ${result[0]} 年龄: ${result[1]}`)
+  //   process.exit(0)
+  // })
 
-  // 下载模板
-  await download(repo, name, { clone: true }, (err) => {
-    if (err) return log('模板下载出错')
+  // const readline = require('readline')
+  // let selected = 0
+  // const choices = ['javascript', 'css', 'html']
+  // let lineCount = 0
+  // const rl = readline.createInterface(process.stdin, process.stdout)
+  // function reader() {
+  //   let str = ''
+  //   for (let i = 0; i < choices.length; i++) {
+  //     lineCount++
+  //     str += `${selected === i ? '[X]' : '[ ]'} ${choices[i]}\r\n`
+  //   }
+  //   process.stdout.write(str)
+  // }
 
-    process.succeed()
-    log(' 🎉下载成功 🎉')
+  // reader()
 
-    // 安装依赖
-    log('👉 👉 安装依赖')
-    execa.commandSync('cnpm install', {
-      cwd: `./${name}`,
-      stdio: ['inherit', 'inherit', 'inherit'],
+  // process.stdin.on('keypress', (s, key) => {
+  //   const name = key.name
+  //   const max = choices.length
+  //   if (name === 'up' && selected > 0) {
+  //     selected--
+  //   } else if (name === 'down' && selected < max - 1) {
+  //     selected++
+  //   } else if (name === 'down' && selected === max - 1) {
+  //     selected = 0
+  //   } else if (name === 'up' && selected === 0) {
+  //     selected = max - 1
+  //   } else {
+  //     return true
+  //   }
+  //   // 移动光标至起始位置，确保后续输入覆盖当前内容
+  //   readline.moveCursor(process.stdout, 0, -lineCount)
+  //   lineCount -= choices.length
+  //   reader()
+  // })
+
+  // rl.on('line', () => {
+  //   console.log(`you choose ${choices[selected]}`)
+  //   process.exit(0)
+  // }).on('close', () => {
+  //   rl.close()
+  // })
+
+  const yargs = require('yargs/yargs')
+  const { hideBin } = require('yargs/helpers')
+
+  yargs(hideBin(process.argv))
+    .command(
+      'serve [port]',
+      'start the server',
+      (yargs) => {
+        return yargs.positional('port', {
+          describe: 'port to bind on',
+          default: 5000,
+        })
+      },
+      (argv) => {
+        if (argv.verbose) console.info(`start server on :${argv.port}`)
+        serve(argv.port)
+      }
+    )
+    .option('verbose', {
+      alias: 'v',
+      type: 'boolean',
+      description: 'Run with verbose logging',
     })
+    .parse()
 
-    log(' 🎉安装依赖成功 🎉')
+  // const repo =
+  //   'direct:https://bitbucket.org/JeremyYu_1122/vue-test/get/master.zip'
 
-    open('http://localhost:3000/')
+  // const process = ora('下载模板....')
 
-    execa.commandSync('npm run dev', {
-      cwd: `./${name}`,
-      stdio: ['inherit', 'inherit', 'inherit'],
-    })
-  })
+  // process.start()
+
+  // // 下载模板
+  // await download(repo, name, { clone: true }, (err) => {
+  //   if (err) return log('模板下载出错')
+
+  //   process.succeed()
+  //   log(' 🎉下载成功 🎉')
+
+  //   // 安装依赖
+  //   log('👉 👉 安装依赖')
+  //   execa.commandSync('cnpm install', {
+  //     cwd: `./${name}`,
+  //     stdio: ['inherit', 'inherit', 'inherit'],
+  //   })
+
+  //   log(' 🎉安装依赖成功 🎉')
+
+  //   open('http://localhost:3000/')
+
+  //   execa.commandSync('npm run dev', {
+  //     cwd: `./${name}`,
+  //     stdio: ['inherit', 'inherit', 'inherit'],
+  //   })
+  // })
 }
