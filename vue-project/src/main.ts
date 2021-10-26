09382cb72fdc9a42bb2efb2181cbd16a0aea1adb
互联网-lg <liugang2@bsoft.com.cn>
2021-10-26 13:14:48 +0800

    FIX:10.26修改cliv1.2

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -10,33 +10,3 @@ app.use(router)
 app.use(store)
 
 app.mount('#app')
-
-setTimeout(() => {
-  console.info('1')
-}, 0)
-
-console.info('2')
-
-const s1 = async () => {
-  console.info('3')
-
-  await s2()
-
-  console.info('4')
-}
-
-const s2 = async () => {
-  console.info('5')
-}
-
-s1()
-
-new Promise(function (resolve) {
-  console.log('6')
-  resolve(true)
-  console.log('7')
-}).then(function () {
-  console.log('8')
-})
-
-console.info('9')
